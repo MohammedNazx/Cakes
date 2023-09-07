420e5da2a6913a318286a160f8e0659d6bf603dd
Mohammed Nazamul Hassan <hassan40@sheridan.ca>
2023-09-07 01:46:23 -0400

    getting users data

--- cart.php
+++ cart.php
@@ -6,11 +6,27 @@ include("./db.php");
 
 if (isset($_POST['test'])) {
 
-    $sql = "INSERT INTO item (item_name) VALUES ('cake1')";
+    $user_id = $_SESSION['user_id'];
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    // SQL Command
+    $user = "SELECT * FROM `user` WHERE id = $user_id ";
+
+    $result = $conn->query($user);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"] . " - Name: " . $row["name"] . " " . $row["email"] . "<br>";
+        }
     }
 }
+
+
+
+//$sql = "INSERT INTO item (item_name) VALUES ('cake1')";
+
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "New record created successfully";
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
