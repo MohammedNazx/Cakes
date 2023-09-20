734069f30f1648fb4c4a067bcdbd8c18687a7048
Mohammed Nazamul Hassan <hassan40@sheridancollege.ca>
2023-09-20 16:55:39 -0400

    added login page

--- Login/login.php
+++ Login/login.php
@@ -85,40 +85,65 @@ if (isset($_SESSION['user_login']) && $_SESSION['user_login'] == true) {
 
 <body>
 
-    <form method="POST" action="login.php">
 
-        <table>
 
-            <tr>
-                <td> Email: </td>
-                <td>
-                    <input type="text" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : null; ?>">
-                </td>
-            <tr>
-            <tr>
-                <td></td>
-                <td style="color:red;">
-                    <?php echo (isset($error['user_email'])) ? $error['user_email'] : ''; ?>
-                </td>
-            </tr>
+    <section class="vh-100" style="background-color: #9A616D;">
+        <div class="container py-5 h-100">
 
-            <tr>
-                <td> Password : </td>
-                <td> <input type="text" name="password" value="<?php echo (isset($_POST['password'])) ? $_POST['password'] : null; ?>"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td style="color:red;">
-                    <?php echo (isset($error['password'])) ? $error['password'] : ''; ?>
-                </td>
-            </tr>
+            <div class="row d-flex justify-content-center align-items-center h-100">
 
-        </table>
-        <button type="submit">Sign in</button>
-        <br>
-        <br>
+                <div class="col col-xl-10">
 
-    </form>
+                    <div class="card" style="border-radius: 1rem;">
+
+                        <div class="row g-0">
+
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                            </div>
+
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+
+                                    <form action="login.php" method="POST">
+
+                                        <!-- LOGO AREA -->
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                            <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                            <span class="h1 fw-bold mb-0">Logo</span>
+                                        </div>
+
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="email" name="email" class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example17">Email address</label>
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <input type="password" name="password"  class="form-control form-control-lg" />
+                                            <label class="form-label" for="form2Example27">Password</label>
+                                        </div>
+
+                                        <div class="pt-1 mb-4">
+                                            <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                                        </div>
+
+                                        <a class="small text-muted" href="#!">Forgot password?</a>
+                                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!" style="color: #393f81;">Register here</a></p>
+                                        <a href="#!" class="small text-muted">Terms of use.</a>
+                                        <a href="#!" class="small text-muted">Privacy policy</a>
+                                    </form>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
--- index.php
+++ index.php
@@ -64,6 +64,11 @@ $wedding = $conn->query($sql_wedding);
                         <a class="nav-link" href="">Order</a>
                     </li>
 
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Cart</a>
+                    </li>
+
+
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             More
